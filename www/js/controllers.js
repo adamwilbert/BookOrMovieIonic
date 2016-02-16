4eb9bcf7d7e8fde7567861e3dfda2749f9b111f8
Adam Wilbert <adam.wilbert@gmail.com>
2016-02-16 09:11:44 -0800

    continuing oauth

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['LocalStorageModule', 'ionic'])
+angular.module('starter.controllers', ['LocalStorageModule', 'ionic', 'ngCordova'])
 
 .controller('MainController', function($scope) {
   $scope.secret = 'Espeon is bae'
@@ -39,11 +39,16 @@ angular.module('starter.controllers', ['LocalStorageModule', 'ionic'])
       }
     }
     vm.movieVote = function(){
-      $http.put(mainPropUrl + $stateParams.propertyId, {userId: 'Movie'})
+      $http.put(mainPropUrl + 'movieVote/'+ $stateParams.propertyId, {"userId": '56be8aa8899c5bdd8533bcfc'})
         .success(function(data){
+          console.log(data)
         });
     }
     vm.bookVote = function(){
+      $http.put(mainPropUrl + 'bookVote/'+ $stateParams.propertyId, {"userId": '56be8aa8899c5bdd8533bcfc'})
+        .success(function(data){
+          console.log(data)
+        });
 
     }
 
@@ -61,7 +66,7 @@ angular.module('starter.controllers', ['LocalStorageModule', 'ionic'])
         });
 })
 
-.controller('UserCtrl', function($scope, $http, localStorageService, GoogleLoginService) {
+.controller('UserCtrl', function($scope, $http, localStorageService) {
     var vm = this
     vm.test = 'espeon is bae'
     $scope.localstorage = localStorageService
@@ -71,20 +76,43 @@ angular.module('starter.controllers', ['LocalStorageModule', 'ionic'])
     }
 })
 
-.controller('AuthenticationController', function($scope, $state, localStorageService, GoogleLoginService, $http){
-  $scope.test = 'espeon is bae'
-    // if(localStorageService.get('google-token')){
-    //   $http.get('http://localhost:8080/users/show')
-    //   User.get({id: localStorageService.get('userId')}), function(user){
-    //     $state.transitionTo('dash')
-    //     $scope.Authenticated = true
-    //   }
-    // } else {
-    //   $scope.needsAuthentication = true
-    // }
-  $scope.googlelogin = GoogleLoginService.login
-  $scope.logout = function(){
-      localStorageService.clearAll()
-      location.href = location.pathname
-    }
-})
+.controller('OauthController', ['$scope', '$cordovaOauth', '$http', function($scope, $cordovaOauth, $http){
+  $scope.googleLogin = function(){
+    $cordovaOauth.google("805387380544-copmhikv3sg6cd36gsp949nugdol3hva.apps.googleusercontent.com",
+      ["https://www.googleapis.com/auth/urlshortener", "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/plus.me"]).
+    then(function(result){
+      console.log("google login success");
+      var accessToken;
+      //$location.url('/scan');
+      console.log(JSON.stringify(result));
+      accessToken = JSON.stringify(result);
+      console.log(result.access_token);
+      console.log(typeof(result.access_token));
+
+      //getting profile info of the user
+      $http({method:"GET", url:"https://www.googleapis.com/plus/v1/people/me?access_token="+result.access_token}).
+      success(function(response){
+               console.log(response);
+              var param = {
+                provider: 'google',
+                  google: {
+                                uid: response["id"],
+                                provider: 'google',
+                                first_name: response["name"]["givenName"],
+                                last_name: response["name"]["familyName"],
+                                email: response.emails[0]["value"],
+                                image: response.image.url
+                            }
+                };
+                console.log(param);
+      }, function(error) {
+      console.log(error);
+    });
+
+  }, function(error){
+    console.log(error);
+  });
+}
+
+}])

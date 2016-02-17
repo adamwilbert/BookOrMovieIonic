659137d66e97b87c4f86b22c62dd687b49c4ef35
Adam Wilbert <adam.wilbert@gmail.com>
2016-02-17 08:53:19 -0800

    jsconsole mobile

--- www/lib/ng-cordova-oauth/src/oauth.google.js
+++ www/lib/ng-cordova-oauth/src/oauth.google.js
@@ -24,7 +24,7 @@ function google($q, $http, $cordovaOauthUtility) {
         }
         var browserRef = window.cordova.InAppBrowser.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&approval_prompt=force&response_type=token', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
         browserRef.addEventListener("loadstart", function(event) {
-          if((event.url).indexOf(redirect_uri) >-1) {
+          if((event.url).indexOf(redirect_uri) === 0 ) {
             browserRef.removeEventListener("exit",function(event){});
             browserRef.close();
             var callbackResponse = (event.url).split("#")[1];
